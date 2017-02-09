153d078090e2a23e536960d415b45e009d35b391
Steve Brownlee <chortlehoort@gmail.com>
2017-02-09 12:22:00 -0600

    Trying the Peewee slim ORM for db interactions

--- lootbag.py
+++ lootbag.py
@@ -1,58 +1,51 @@
 import sys
 import sqlite3
+from peewee import *
+
+db = SqliteDatabase('bagoloot.db')
+
+class Child(Model):
+    name = CharField()
+
+    class Meta:
+        database = db
+
+class Toy(Model):
+    name = CharField()
+    child = ForeignKeyField(Child, related_name="toys")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        database = db
 
 class LootBag():
 
     def __init__(self):
-        self.good_children = dict()
+        try:
+            db.connect()
+            db.create_tables([Child, Toy])
+        except OperationalError:
+            pass
 
     def add_toy_for_child(self, child, toy):
+        try:
+            kid = Child.select().where(Child.name == child).get()
+        except Exception as ex:
+            kid = Child.create(name=child)
 
-        with sqlite3.connect('lootbag.db') as conn:
-            c = conn.cursor()
-
-            try:
-                c.execute("INSERT INTO Child VALUES (?, ?, ?)",
-                    (None, child, 0))
-            except sqlite3.OperationalError:
-                pass
-
-            c.execute("SELECT ChildId FROM Child WHERE Name='{}'".format(child))
-            results = c.fetchall()
-
-            try:
-                c.execute("INSERT INTO Toy VALUES (?, ?, ?)",
-                    (None, toy, results[0][0]))
-            except sqlite3.OperationalError:
-                pass
+        the_toy = Toy.create(name=toy, child=kid)
 
     def remove_toy_for_child(self, child, toy):
-        with sqlite3.connect('lootbag.db') as conn:
-            c = conn.cursor()
-
-            c.execute("SELECT ChildId FROM Child WHERE Name='{}'".format(child))
-            results = c.fetchall()
-
-            try:
-                c.execute("DELETE FROM Toy WHERE ChildId={} AND Name='{}'"
-                    .format(results[0][0], toy))
-            except sqlite3.OperationalError:
-                pass
-
+        kid = Child.select().where(Child.name == child).get()
+        t = Toy.select().where(Toy.child == kid).get()
+        t.delete_instance()
 
     def get_by_child(self, child):
-        with sqlite3.connect('lootbag.db') as conn:
-            c = conn.cursor()
-
-            c.execute("""SELECT t.Name
-                FROM Toy t, Child c
-                WHERE c.Name='{}'
-                AND c.ChildId = t.ChildId
-            """.format(child))
-
-            toys = c.fetchall()
-            print(toys)
-
+        kid = Child.select().where(Child.name == child).get()
+        toys = Toy.select().where(Toy.child == kid)
+        return [toy.name for toy in toys]
 
     def get_list_of_kids(self):
         return [kid for kid in self.good_children.keys()]
@@ -63,6 +56,17 @@ class LootBag():
     def deliver_toys_to_child(self, child):
         self.good_children[child]["delivered"] = True
 
+    def report(self):
+        toy_ct = fn.Count(Toy.id)
+        
+        children = (Child
+                .select(Child, toy_ct.alias('toy_count'))
+                .join(Toy, JOIN.LEFT_OUTER)
+                .group_by(Child)
+                .order_by(toy_ct.desc()))
+
+        [print("{} has {} toys".format(child.name, child.toy_count)) for child in children]
+
 
 
 
@@ -70,12 +74,14 @@ class LootBag():
 if __name__ == "__main__":
     bag = LootBag()
     if sys.argv[1] == "add":
-      bag.add_toy_for_child(sys.argv[3], sys.argv[2])
-      print(bag.get_by_child(sys.argv[3]))
+      bag.add_toy_for_child(sys.argv[2], sys.argv[3])
+      print(bag.get_by_child(sys.argv[2]))
     elif sys.argv[1] == "remove":
       bag.remove_toy_for_child(sys.argv[2], sys.argv[3])
     elif sys.argv[1] == "ls":
-      bag.get_by_child(sys.argv[2])
+      print(bag.get_by_child(sys.argv[2]))
+    elif sys.argv[1] == "report":
+      bag.report()
 
 
 
